a4db027f499aeae3a8cffd2c77b16c2cac8a816a
Andrei Gorshkov <andruhinsans@mail.ru>
2024-09-28 01:29:25 +0300

    first commit

---
+++ src/main/java/CalculatorController.java
@@ -0,0 +1,12 @@
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CalculatorController {
+    private final CalculatorService calculatorService;
+
+    public CalculatorController(CalculatorService calculatorService) {
+        this.calculatorService = calculatorService;
+    }
+
+
+}
---
+++ src/main/java/CalculatorService.java
@@ -0,0 +1,9 @@
+import org.springframework.stereotype.Service;
+
+@Service
+public class CalculatorService {
+    public String greeting() {
+        return "Добро пожаловать в калькулятор";
+    }
+
+}
